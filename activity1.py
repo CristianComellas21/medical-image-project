7cd147ced05ff1fe98c3d6d4f26cfd88e046d1ff
CristianComellas21 <ccf200@hotmail.com>
2024-04-25 20:43:30 +0200

    Read segmentation

--- activity1.py
+++ activity1.py
@@ -2,13 +2,13 @@ import numpy as np
 import pydicom as dicom
 
 DICOM_FILES_PATH = "data/HCC-TACE-Seg/HCC_003"
+SEGMENTATION_PATH = f"{DICOM_FILES_PATH}/09-12-1997-NA-AP LIVER-64595/300.000000-Segmentation-45632/1-1.dcm"
 
 
 def main():
-    file = dicom.read_file(
-        f"{DICOM_FILES_PATH}/10-31-1997-NA-CT ABDOMEN LIVER-88989/4.000000-Recon 2 3 PHASE LIVER ABD-94194/2-103.dcm"
-    )
-    print(file)
+    segmentation_dicom = dicom.read_file(SEGMENTATION_PATH)
+    print(segmentation_dicom)
+    print(segmentation_dicom.pixel_array.shape)
 
 
 if __name__ == "__main__":
