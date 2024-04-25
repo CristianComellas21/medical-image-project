233e26c5349ea271bebbcfc3464d40bcd4258b0e
CristianComellas21 <ccf200@hotmail.com>
2024-04-25 20:18:34 +0200

    Read first dicom

--- activity1.py
+++ activity1.py
@@ -1,8 +1,14 @@
 import numpy as np
+import pydicom as dicom
+
+DICOM_FILES_PATH = "data/HCC-TACE-Seg/HCC_003"
 
 
 def main():
-    pass
+    file = dicom.read_file(
+        f"{DICOM_FILES_PATH}/10-31-1997-NA-CT ABDOMEN LIVER-88989/4.000000-Recon 2 3 PHASE LIVER ABD-94194/2-103.dcm"
+    )
+    print(file)
 
 
 if __name__ == "__main__":
