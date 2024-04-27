2af68cc85f53625b7eb42bafa09fa029c6b56d02
CristianComellas21 <ccf200@hotmail.com>
2024-04-27 14:02:52 +0200

    Visualizing first read dicom

--- activity1.py
+++ activity1.py
@@ -1,6 +1,6 @@
-import numpy as np
-import pydicom as dicom
+from pathlib import Path
 
+from dicom import read_dicom_files
 from visualize import plot_interactive_dicom
 
 # Define constants for the paths to the DICOM files
@@ -13,18 +13,17 @@ DICOM_PATH = f"{DICOM_FOLDER}/09-12-1997-NA-AP LIVER-64595/4.000000-Recon 2 LIVE
 
 def main():
 
-    # Load the segmentation DICOM file
-    segmentation_dicom = dicom.read_file(SEGMENTATION_PATH)
-    # print(segmentation_dicom)
-    # print(segmentation_dicom.pixel_array.shape)
-    plot_interactive_dicom(segmentation_dicom.pixel_array)
+    # Load all DICOM files in the folder
+    dicom_folder = Path(f"{DICOM_FOLDER}/09-12-1997-NA-AP LIVER-64595")
 
-    # Load the first DICOM file
-    dicom_files = dicom.dcmread(DICOM_PATH)
-    # print(dicom_files)
-    # print(dicom_files.pixel_array.shape)
+    dicom_files = read_dicom_files(dicom_folder)
 
-    # Create a mask from the segmentation DICOM file
+    # Get 1 DICOM data
+    dicom_data = dicom_files["4.000000-Recon 2 LIVER 3 PHASE AP-18688"][1]
+    pixel_array = dicom_data["pixel_array"]
+
+    # Plot the DICOM files
+    plot_interactive_dicom(pixel_array)
 
 
 if __name__ == "__main__":
