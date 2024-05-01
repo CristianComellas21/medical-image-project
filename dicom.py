d0e3d3c6e3fa9e10de5aef9417ac12732c2c319d
CristianComellas21 <ccf200@hotmail.com>
2024-05-01 16:40:27 +0200

    Add get segmentation layers

--- dicom.py
+++ dicom.py
@@ -4,52 +4,6 @@ import numpy as np
 import pydicom as dicom
 
 
-def concat_dicom_files(dicom_files: [dicom.FileDataset]) -> dict:
-    """Concatenate a list of DICOM files into a single DICOM file."""
-    # print(dicom_files)
-    # print(len(dicom_files))
-
-    if len(dicom_files) == 1:
-        pixel_array = dicom_files[0].pixel_array
-        del dicom_files[0].PixelData
-        return {
-            "pixel_array": pixel_array,
-            "metadata": [dicom_files[0]],
-        }
-
-    # Get the first DICOM file in the list
-    dicom_file = dicom_files[0]
-
-    # Get the pixel array from the first DICOM file
-    pixel_array = dicom_file.pixel_array
-
-    # Get the shape of the pixel array
-    shape = pixel_array.shape
-
-    # Create an empty array to store the concatenated pixel array
-    concatenated_pixel_array = np.empty((len(dicom_files), shape[0], shape[1]))
-
-    # Create empty list to store metadata
-    metadata = []
-
-    # Loop through all DICOM files in the list
-    for idx, dicom_file in enumerate(dicom_files):
-
-        # Get the pixel array from the DICOM file
-        pixel_array = dicom_file.pixel_array
-
-        # Store the pixel array in the concatenated pixel array
-        concatenated_pixel_array[idx] = pixel_array
-
-        # Remove the pixel array from the DICOM file
-        del dicom_file.PixelData
-
-        # Store metadata
-        metadata.append(dicom_file)
-
-    return {"pixel_array": concatenated_pixel_array, "metadata": metadata}
-
-
 def read_dicom_files(dicom_folder: Path) -> dict[str, dicom.FileDataset]:
     """Read all DICOM files in a folder and return them as a dictionary."""
 
@@ -94,7 +48,7 @@ def read_dicom_files(dicom_folder: Path) -> dict[str, dicom.FileDataset]:
             # If the current dicom index is not the same as the previous one
             # then we have a new dicom series
             if current_dicom_idx != prev_dicom_idx:
-                current_dict[prev_dicom_idx] = concat_dicom_files(dicom_data)
+                current_dict[prev_dicom_idx] = __concat_dicom_files(dicom_data)
                 dicom_data = []
                 prev_dicom_idx = current_dicom_idx
 
@@ -102,6 +56,88 @@ def read_dicom_files(dicom_folder: Path) -> dict[str, dicom.FileDataset]:
             dicom_data.append(dicom.read_file(dicom_file))
 
         # Concatenate the last dicom series
-        current_dict[prev_dicom_idx] = concat_dicom_files(dicom_data)
+        current_dict[prev_dicom_idx] = __concat_dicom_files(dicom_data)
 
     return dicom_files
+
+
+def get_segmentation_layers(
+    segmentation_pixel_array: np.ndarray, segmentation_metadata: dicom.FileDataset
+) -> dict[str, np.ndarray]:
+    """Get the segmentation layers from a DICOM segmentation file."""
+
+    # Create an empty dictionary to store the segmentation layers
+    layers = {}
+
+    # Get the number of sequences and elements per sequence
+    n_sequences = len(segmentation_metadata.SegmentSequence)
+    n_elements_per_sequence = segmentation_pixel_array.shape[0] // n_sequences
+
+    # Loop through all sequences in the segmentation metadata
+    for sequence in segmentation_metadata.SegmentSequence:
+
+        # Get the name of the sequence and the segment number
+        name = sequence.SegmentDescription.lower()
+        segment_number = sequence.SegmentNumber
+
+        # Get the pixel array for the sequence
+        pixel_array = segmentation_pixel_array[
+            (segment_number - 1)
+            * n_elements_per_sequence : segment_number
+            * n_elements_per_sequence
+        ]
+
+        # Store the pixel array in the dictionary along with the name and number
+        layers[name] = {
+            "pixel_array": pixel_array,
+            "name": sequence.SegmentLabel,
+            "num": sequence.SegmentNumber,
+        }
+
+    return layers
+
+
+def __concat_dicom_files(dicom_files: [dicom.FileDataset]) -> dict:
+    """Concatenate a list of DICOM files into a single DICOM file."""
+    # print(dicom_files)
+    # print(len(dicom_files))
+
+    if len(dicom_files) == 1:
+        pixel_array = dicom_files[0].pixel_array
+        del dicom_files[0].PixelData
+        return {
+            "pixel_array": pixel_array,
+            "metadata": [dicom_files[0]],
+        }
+
+    # Get the first DICOM file in the list
+    dicom_file = dicom_files[0]
+
+    # Get the pixel array from the first DICOM file
+    pixel_array = dicom_file.pixel_array
+
+    # Get the shape of the pixel array
+    shape = pixel_array.shape
+
+    # Create an empty array to store the concatenated pixel array
+    concatenated_pixel_array = np.empty((len(dicom_files), shape[0], shape[1]))
+
+    # Create empty list to store metadata
+    metadata = []
+
+    # Loop through all DICOM files in the list
+    for idx, dicom_file in enumerate(dicom_files):
+
+        # Get the pixel array from the DICOM file
+        pixel_array = dicom_file.pixel_array
+
+        # Store the pixel array in the concatenated pixel array
+        concatenated_pixel_array[idx] = pixel_array
+
+        # Remove the pixel array from the DICOM file
+        del dicom_file.PixelData
+
+        # Store metadata
+        metadata.append(dicom_file)
+
+    return {"pixel_array": concatenated_pixel_array, "metadata": metadata}
